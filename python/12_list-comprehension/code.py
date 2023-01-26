66c933bf7b73ca3d7c2f9676cf3fea2f7a959d7b
Allen K Abraham <allenkabraham777@gmail.com>
2023-01-26 19:01:06 +0530

    List comprehension

---
+++ python/12_list-comprehension/code.py
@@ -0,0 +1,15 @@
+numbers = [1, 3, 5]
+doubled = [num * 2 for num in numbers]
+
+print(doubled)
+
+friends = ["Rolf", "Sam", "Samantha", "Saurabh", "Jen"]
+starts_s = []
+
+for friend in friends:
+  if friend.startswith("S"):
+    starts_s.append(friend)
+print(starts_s)
+
+starts_s = [friend for friend in friends if friend.startswith("S")]
+print(starts_s)

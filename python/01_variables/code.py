f2869c927c0543f82152e1b281bb1f471bbe2178
allen.abraham <allen.abraham@themathcompany.com>
2023-01-26 18:02:15 +0530

    Variables

---
+++ python/01_variables/code.py
@@ -0,0 +1,33 @@
+# Numbers
+print("======= Numbers =======")
+x = 15
+price  = 9.99
+
+discount = 0.2
+
+result = price * ( 1 - discount )
+
+print(result)
+print("=====================")
+
+# String
+print("======= String =======")
+name = "John"
+print(name)
+
+name = 'Bob' # Reassign
+print(name)
+print(name * 2)
+print("=====================")
+
+# Changing variable values
+print("======= Variable =======")
+a = 25
+b = a
+print("a = " +  str(a))
+print("b = " +  str(b))
+
+b = 17
+print("a = " +  str(a))
+print("b = " +  str(b))
+print("=====================")

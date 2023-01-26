07b129801c5b11a2b0535d35be71000aa9ba6142
allen.abraham <allen.abraham@themathcompany.com>
2023-01-26 18:04:03 +0530

    Boolean

---
+++ python/07_booleans/code.py
@@ -0,0 +1,11 @@
+print(5 == 5)
+print(5 > 5)
+print(10 != 9)
+
+# Comparisions; ==, !=, >, <, >=, <=
+
+friend = ["Rolf", "Bob"]
+abroad = ["Rolf", "Bob"]
+
+print(friend == abroad) # True
+print(friend is abroad) # False

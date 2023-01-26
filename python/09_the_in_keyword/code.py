8d5ece96ccccdc8c18d65b84609c4b5141d8a7f8
allen.abraham <allen.abraham@themathcompany.com>
2023-01-26 18:04:28 +0530

    In keyword

---
+++ python/09_the_in_keyword/code.py
@@ -0,0 +1,12 @@
+friends = ["Bob", "Rob", "Jen"]
+
+print("Jen" in friends)
+
+
+movies = {"The Matrix", "Green Book", "Her"}
+user_movie = input("Enter a movie: ")
+
+print(user_movie in movies)
+
+
+print("abc" in "abcdef")

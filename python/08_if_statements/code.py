936a641d351fbb276a3269abe20bac229d86c86b
allen.abraham <allen.abraham@themathcompany.com>
2023-01-26 18:04:17 +0530

    If statement

---
+++ python/08_if_statements/code.py
@@ -0,0 +1,10 @@
+day_of_week = input("Enter day: ").lower()
+
+if day_of_week == "monday":
+  print("Have a great start to your week!")
+elif day_of_week == "tuesday":
+  print("Day is Tuesday")
+else:
+  print("Full speed ahead!")
+
+print("This always run.")
